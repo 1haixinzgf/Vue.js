07aa5914d402318dac147071290fb6cc1d09bd45
unknown <2725965600@qq.com>
2018-06-28 20:04:34 +0800

    优化页面跳转函数

--- element-ui-admin/src/components/Comment.vue
+++ element-ui-admin/src/components/Comment.vue
@@ -4,6 +4,7 @@
         </comment-form>
         <comment-lists :list="list">
         </comment-lists>
+        <pagenation :totalCount="totalCount" :currentPage="currentPage" :pagesize="pagesize" @turnPage="turnPage"></pagenation>
     </div>
 </template>
 
@@ -24,29 +25,21 @@ export default {
         }
     },
     methods: {
-        addComment(msg) {
-            this.list.reverse();
-            this.totalData.push({'text': msg});
+        addComment(msg) {       
+            this.totalData.unshift({'text': msg});
             this.totalCount = this.totalData.length;
             if (this.totalCount < this.pagesize) {
             this.list = this.totalData;
             } 
             else {
-            this.list = this.totalData.slice(this.totalCount - this.pagesize);             
+            this.list = this.totalData.slice(0, this.pagesize);             
             }
             this.currentPage = 1;
-            this.list.reverse();
         },
         turnPage(curr) {
-            this.totalData = this.totalData.reverse()
             this.currentPage = curr;
-            let num = 3*curr
-            if(this.totalCount-3*curr > 0) {
-                this.list = this.totalData.slice(num - 3,num);
-            } else {
-                this.list = this.totalData.slice(num - 3);
-            }
-            this.totalData = this.totalData.reverse()
+            let num = this.pagesize*curr  
+            this.list = this.totalData.slice(num - this.pagesize,num);
         }
     },
     components: {
